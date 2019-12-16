244930a45ef2b0e08e5d68b2d986e685e1490091
csrene <renecoutoesilva@gmail.com>
2019-12-15 22:54:40 -0300

    great success...

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ use std::thread;
 use std::net::{UdpSocket, SocketAddr};
 use std::fmt::UpperExp;
 use std::num::ParseIntError;
+use std::sync::mpsc::{channel, Sender, Receiver};
+use std::time::Duration;
+use std::thread::JoinHandle;
+
+#[derive(Debug)]
+struct Response(String);
 
 #[derive(Debug)]
 struct Record(Vec<u8>);
@@ -19,7 +25,6 @@ struct ShardId(String);
 
 struct ShardWriter {
     pub p_shard_id: ProtectedShardId,
-//    pub partition_id: String
 }
 
 // We use an Arc to distribute the references with other threads, we use a mutex to make it so only one thread ever has acces to the file each time
@@ -36,10 +41,7 @@ struct ShardReader {
 fn build_record(base64_data: &[u8]) -> Record {
     let mut res = Vec::with_capacity(base64_data.len() + 1);
     base64_data.iter().for_each(|b| res.push(b.clone()));
-//    res.clone_from_slice(&base64_data);
     res.push(b'\n');
-//    let mut line = std::str::from_utf8(base64_data);
-//    line.push_str("\n");
     Record(res)
 }
 
@@ -126,42 +128,51 @@ struct UdpServer {
 impl UdpServer {
     fn default() -> Self {
         let socket = UdpSocket::bind("127.0.0.1:3400").expect("couldn't bind to address");
+        socket.set_read_timeout(Some(Duration::from_millis(10))).expect("couldn't set read timeout");
         UdpServer { socket }
     }
 
-    fn poll_request(&self) -> Result<(Option<Request>, SocketAddr), String> {
+    fn poll_request(&self) -> Option<Result<(Option<Request>, SocketAddr), (String, SocketAddr)>> {
         let mut buf = [0; 500];
-        let (number_of_bytes, src_addr) = self.socket.recv_from(&mut buf).expect("Didn't receive data");
+        let recv = self.socket.recv_from(&mut buf);
 
-        let filled_buf = &mut buf[..number_of_bytes];
-
-        let request = std::str::from_utf8(filled_buf).map_err(|x| x.to_string())?;
-        let mut request_split: Vec<&str> = request.split_whitespace().collect();
-        match request_split[..] {
-            ["GetShardIterator", "Latest"] => Ok((Some(Request::GetShardIterator(ShardIteratorType::Latest)), src_addr)),
-            ["GetShardIterator", "Oldest"] => Ok((Some(Request::GetShardIterator(ShardIteratorType::Oldest)), src_addr)),
-            ["GetRecords", x] => {
-                let shit: u64  = x.parse().map_err(|err: ParseIntError| err.to_string())?;
-                Ok((Some(Request::GetRecords(shit)), src_addr))
-            },
-            ["PutRecords", base64_stuff] => {
-                match base64::decode(base64_stuff) {
-                    Err(e) => Err("invalid data. It must be base64 encoded".to_string()),
-                    Ok(_) => {
-                        let records: Vec<u8>   = base64_stuff.as_bytes().to_vec();
-                        Ok((Some(Request::PutRecords(records)), src_addr))
-                    }
-                }
-
-            },
-            [] => Ok((None, src_addr)),
-            _ => Err("invalid request".to_string())
+        if let Err(e) = recv {
+            return None
         }
+
+        let (number_of_bytes, src_addr) = recv.unwrap();
+
+        let mut result = move ||{
+            let filled_buf = &mut buf[..number_of_bytes];
+
+            let request = std::str::from_utf8(filled_buf).map_err(|x| (x.to_string(), src_addr))?;
+            let mut request_split: Vec<&str> = request.split_whitespace().collect();
+            match request_split[..] {
+                ["GetShardIterator", "Latest"] => Ok((Some(Request::GetShardIterator(ShardIteratorType::Latest)), src_addr)),
+                ["GetShardIterator", "Oldest"] => Ok((Some(Request::GetShardIterator(ShardIteratorType::Oldest)), src_addr)),
+                ["GetRecords", x] => {
+                    let shit: u64  = x.parse().map_err(|err: ParseIntError| (err.to_string(), src_addr))?;
+                    Ok((Some(Request::GetRecords(shit)), src_addr))
+                },
+                ["PutRecords", base64_stuff] => {
+                    match base64::decode(base64_stuff) {
+                        Err(e) => Err(("invalid data. It must be base64 encoded".to_string(), src_addr)),
+                        Ok(_) => {
+                            let records: Vec<u8>   = base64_stuff.as_bytes().to_vec();
+                            Ok((Some(Request::PutRecords(records)), src_addr))
+                        }
+                    }
+
+                },
+                [] => Ok((None, src_addr)),
+                _ => Err(("invalid request".to_string(), src_addr))
+            }
+        };
+
+        Some(result())
     }
 }
 
-#[derive(Debug)]
-struct Response(String);
 
 fn handle_request(p_shard_id: ProtectedShardId, request: Request) -> Result<Response, String> {
     match request {
@@ -195,61 +206,77 @@ fn handle_request(p_shard_id: ProtectedShardId, request: Request) -> Result<Resp
     }
 }
 
-// single-thread with udp server
-fn main3() {
+
+fn start_shard_workers(
+    p_shard_id: ProtectedShardId,
+    task_rx: Arc<Mutex<Receiver<(Request, SocketAddr)>>>,
+    response_tx: Sender<(Response, SocketAddr)>
+) -> Vec<JoinHandle<()>> {
+    let mut threads = Vec::new();
+    for n in 0..8 {
+        let p_shard_id = p_shard_id.clone();
+        let task_rx = task_rx.clone();
+        let response_tx = response_tx.clone();
+        let x = thread::spawn(move || {
+            println!("started thread {}", n);
+            loop {
+                let (req, addr) = {
+                    let rx = task_rx.lock().unwrap();
+                    println!("got rx lock in thread {}", n);
+                    rx.recv().unwrap()
+
+                };
+
+                println!("got req in thread {}", n);
+                let response = handle_request(p_shard_id.clone(), req).unwrap();
+                dbg!(&response);
+                println!("sending from thread {}", n);
+                if let Err(e) = response_tx.send((response, addr)) {
+                    println!("got err: {:?}", e);
+                }
+
+//                println!("waiting for udp lock on thead {}", n);
+            }
+
+        });
+        threads.push(x);
+    }
+    threads
+}
+
+// multi-threaded with udp server on a single thread
+
+fn main() {
     let udp_server = UdpServer::default();
     let shard_id = ShardId("/home/pessoal/code/rinites/samples/1".to_string());
     let p_shard_id = Arc::new(Mutex::new(shard_id));
+
+
+    let (task_tx, rx) = channel();
+
+    let task_rx = Arc::new(Mutex::new(rx));
+
+    let (response_tx, response_rx) = channel();
+
+    let threads = start_shard_workers(p_shard_id, task_rx, response_tx);
+
+
+
     loop {
         match udp_server.poll_request() {
-            Ok((Some(req), addr)) => {
-                let response = handle_request(p_shard_id.clone(), req).unwrap();
-                dbg!(&response);
-
-                udp_server.socket.send_to(response.0.as_bytes(), addr);
+            Some(Ok((Some(req), addr))) => {
+                task_tx.send((req, addr));
             },
-            Err(s) => {dbg!(s);},
+            Some(Err((s, addr))) => {
+                udp_server.socket.send_to(s.as_bytes(), addr);
+                dbg!(s);
+            },
             _ => ()
         }
-    }
-}
 
-
-// multi-threaded with udp server
-
-fn main() {
-    let udp_server = Arc::new(Mutex::new(UdpServer::default()));
-    let shard_id = ShardId("/home/pessoal/code/rinites/samples/1".to_string());
-    let p_shard_id = Arc::new(Mutex::new(shard_id));
-    let mut threads = Vec::new();
-    for n in 0..8 {
-        let udp_server = udp_server.clone();
-        let p_shard_id = p_shard_id.clone();
-        let x = thread::spawn(move || {
-            println!("started thread {}", n);
-
-            loop {
-                let res = {
-                    let udp_s = udp_server.lock().unwrap();
-                    println!("got lock of udp server on thread {}", n);
-                    udp_s.poll_request()
-                };
-                match res {
-                    Ok((Some(req), addr)) => {
-
-                        let response = handle_request(p_shard_id.clone(), req).unwrap();
-                        dbg!(&response);
-                        println!("waiting for udp lock on thead {}", n);
-                        let udp_s = udp_server.lock().unwrap();
-                        println!("got udp lock on thead {}", n);
-                        udp_s.socket.send_to(response.0.as_bytes(), addr);
-                    },
-                    Err(s) => { dbg!(s); },
-                    _ => ()
-                }
-            }
-        });
-        threads.push(x);
+        if let Ok((response, addr)) = response_rx.try_recv() {
+            udp_server.socket.send_to(response.0.as_bytes(), addr);
+        }
     }
 
     for x in threads {
@@ -260,47 +287,114 @@ fn main() {
 
 }
 
-// threaded POC
-fn main2() -> std::io::Result<()> {
-    let UdpServer = UdpServer::default();
-    let shard_id = ShardId("/home/pessoal/code/rinites/samples/1".to_string());
-    let p_shard_id = Arc::new(Mutex::new(shard_id));
-
-    let my_writer = ShardWriter::from_shard(p_shard_id.clone());
-
-    let data = b"some new dataonly not so new!";
-
-
-    my_writer.append(data)?;
-
-    let mut threads = Vec::new();
-    for n in 0..5 {
-        let mut reader: ShardReader = ShardReader { p_shard_id: p_shard_id.clone(), offset: 0, chunk_size: 10 };
-        println!("instantiated reader in thread {}", n);
-        let x = thread::spawn(move || {
-            println!("started thread {}", n);
-            loop {
-                let records: Vec<Record> = reader.read().unwrap();
-                println!("read {} records in thread {}", records.len(), n);
-
-                if records.len() == 0 {
-                    println!("no more records remaining. offset was: {}", reader.offset);
-                    break
-
-                }
-                let text: Vec<&str> = records.iter().map(|r| std::str::from_utf8(&r.0).unwrap()).collect();
-                dbg!(text);
-            }
-
-        });
-
-        threads.push(x);
-
-    }
-
-    for x in threads {
-        x.join().unwrap();
-    }
-
-    Ok(())
-}
+// single-thread with udp server
+//fn main3() {
+//    let udp_server = UdpServer::default();
+//    let shard_id = ShardId("/home/pessoal/code/rinites/samples/1".to_string());
+//    let p_shard_id = Arc::new(Mutex::new(shard_id));
+//    loop {
+//        match udp_server.poll_request() {
+//            Ok((Some(req), addr)) => {
+//                let response = handle_request(p_shard_id.clone(), req).unwrap();
+//                dbg!(&response);
+//
+//                udp_server.socket.send_to(response.0.as_bytes(), addr);
+//            },
+//            Err(s) => {dbg!(s);},
+//            _ => ()
+//        }
+//    }
+//}
+//
+//
+//// multi-threaded with udp server
+//
+//fn main4() {
+//    let udp_server = Arc::new(Mutex::new(UdpServer::default()));
+//    let shard_id = ShardId("/home/pessoal/code/rinites/samples/1".to_string());
+//    let p_shard_id = Arc::new(Mutex::new(shard_id));
+//    let mut threads = Vec::new();
+//    for n in 0..8 {
+//        let udp_server = udp_server.clone();
+//        let p_shard_id = p_shard_id.clone();
+//        let x = thread::spawn(move || {
+//            println!("started thread {}", n);
+//
+//            loop {
+//                // BUG only the first thread ever gets the lock. We might to work with task queues???
+//                let udp_s = udp_server.lock().unwrap();
+//                let res = {
+//
+//                    println!("got lock of udp server on thread {}", n);
+//                    udp_s.poll_request()
+//                };
+//                match res {
+//                    Ok((Some(req), addr)) => {
+//
+//                        let response = handle_request(p_shard_id.clone(), req).unwrap();
+//                        dbg!(&response);
+//                        println!("waiting for udp lock on thead {}", n);
+////                        let udp_s = udp_server.lock().unwrap();
+//                        println!("got udp lock on thead {}", n);
+//                        udp_s.socket.send_to(response.0.as_bytes(), addr);
+//                    },
+//                    Err(s) => { dbg!(s); },
+//                    _ => ()
+//                }
+//            }
+//        });
+//        threads.push(x);
+//    }
+//
+//    for x in threads {
+//        x.join().unwrap();
+//    }
+//
+//    println!("exited");
+//
+//}
+//
+//// threaded POC
+//fn main2() -> std::io::Result<()> {
+//    let UdpServer = UdpServer::default();
+//    let shard_id = ShardId("/home/pessoal/code/rinites/samples/1".to_string());
+//    let p_shard_id = Arc::new(Mutex::new(shard_id));
+//
+//    let my_writer = ShardWriter::from_shard(p_shard_id.clone());
+//
+//    let data = b"some new dataonly not so new!";
+//
+//
+//    my_writer.append(data)?;
+//
+//    let mut threads = Vec::new();
+//    for n in 0..5 {
+//        let mut reader: ShardReader = ShardReader { p_shard_id: p_shard_id.clone(), offset: 0, chunk_size: 10 };
+//        println!("instantiated reader in thread {}", n);
+//        let x = thread::spawn(move || {
+//            println!("started thread {}", n);
+//            loop {
+//                let records: Vec<Record> = reader.read().unwrap();
+//                println!("read {} records in thread {}", records.len(), n);
+//
+//                if records.len() == 0 {
+//                    println!("no more records remaining. offset was: {}", reader.offset);
+//                    break
+//
+//                }
+//                let text: Vec<&str> = records.iter().map(|r| std::str::from_utf8(&r.0).unwrap()).collect();
+//                dbg!(text);
+//            }
+//
+//        });
+//
+//        threads.push(x);
+//
+//    }
+//
+//    for x in threads {
+//        x.join().unwrap();
+//    }
+//
+//    Ok(())
+//}
